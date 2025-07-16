a3bbdedfb0c8e8b5496da0509f04141d0c5ee022
Paige MacGregor <paigemacgregor33@gmail.com>
2025-07-16 15:50:06 -0600

    update url

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -21,12 +21,9 @@ import { RiSendPlaneFill } from "react-icons/ri";
 import { SiTypescript, SiRecoil, SiReactquery } from "react-icons/si";
 
 //import images
-import Ataa from '../assets/images/Ataa.png';
-import Elzero from '../assets/images/Elzero.png';
-import Kasper from '../assets/images/Kasper.png';
-import Leon from '../assets/images/Leon.png';
-import SokoNumber from '../assets/images/SokoNumber.png';
-import GlobalShare from '../assets/images/GlobalShare.png';
+import Asrc from '../assets/images/asrc.webp';
+import Agentelite from '../assets/images/agentelite.webp';
+import Maidenvoyage from '../assets/images/maidenvoyage.svg';
 
 // import style
 import style from './App.module.css';
@@ -34,120 +31,78 @@ import clsx from 'clsx';
 
 const skills = [
 	{
-		name: 'HTML 5',
-		icon: <AiFillHtml5 size="25px" color="white" />,
-		cssName: "html"
+		name: '.Net Logo',
+		url: "https://camo.githubusercontent.com/f4c52b575a890c7e67c6541271fc5733506088d19c77ffde6bab3e18e7948536/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f2e4e45542d3543324439313f7374796c653d666f722d7468652d6261646765266c6f676f3d2e6e6574266c6f676f436f6c6f723d7768697465",
 	},
 	{
-		name: 'CSS 3',
-		icon: <BiLogoCss3 size="25px" color="white" />,
-		cssName: "css"
+		name: 'C# Logo',
+		url: "https://camo.githubusercontent.com/e12029f1d6292800c6a63b7c134a199d76f99552944e2000beef21abca451162/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f632532332d2532333233393132302e7376673f7374796c653d666f722d7468652d6261646765266c6f676f3d637368617270266c6f676f436f6c6f723d7768697465"
+	}, 
+	{
+		name: "Laravel Logo",
+		url: "https://camo.githubusercontent.com/839c2b7156d9a4e8f021ae6c539331e84ea18bf0fd0ee15835f0695a838b292e/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f4c61726176656c2d4646324432303f7374796c653d666f722d7468652d6261646765266c6f676f3d6c61726176656c266c6f676f436f6c6f723d7768697465"
 	},
 	{
-		name: 'Java Script',
-		icon: <BiLogoJavascript size="25px" color="white" />,
-		cssName: "java-script"
+		name: "PHP Logo",
+		url: "https://camo.githubusercontent.com/59f1bf1e0c03f98c620e6456751406b0c8dba1ac0590704d93303b45cfe536ab/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f5048502d3737374242343f7374796c653d666f722d7468652d6261646765266c6f676f3d706870266c6f676f436f6c6f723d7768697465"
 	},
 	{
-		name: "TypeScript",
-		icon: <SiTypescript size="25px" color="white" />,
-		cssName: "type-script"
+		name: "Vue.js Logo",
+		url: "https://camo.githubusercontent.com/7ee502068c9b87eb84df40ef974addabb7ad25ff9d53c19ee793555ecd7ea509/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f5675652e6a732d3335343935453f7374796c653d666f722d7468652d6261646765266c6f676f3d767565646f746a73266c6f676f436f6c6f723d344643303844"
 	},
 	{
-		name: 'React',
-		icon: <FaReact size="25px" color="white" />,
-		cssName: "react"
+		name: "SQ Logo",
+		url: "https://camo.githubusercontent.com/4804cde16e9487428ea27793df49b0ada0166115301325bade5532ae7060ed04/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f4d7953514c2d3030354338343f7374796c653d666f722d7468652d6261646765266c6f676f3d6d7973716c266c6f676f436f6c6f723d7768697465"
 	},
 	{
-		name: 'Redux ToolKit',
-		icon: <BiLogoRedux size="25px" color="white" />,
-		cssName: "redux"
-	},
-	{
-		name: 'Redux Recoil',
-		icon: <SiRecoil size="25px" color="white" />,
-		cssName: "recoil"
-	},
-	{
-		name: 'React Query',
-		icon: <SiReactquery size="25px" color="white" />,
-		cssName: "react-query"
-	},
-	{
-		name: 'RTK Query',
-		icon: <BiLogoRedux size="25px" color="white" />,
-		cssName: "redux"
-	},
-	{
-		name: 'Responsive Design',
-		icon: <FaMobileAlt size="25px" color="white" />,
-		cssName: "responsive"
-	},
-	{
-		name: 'Git',
-		icon: <BsGit size="25px" color="white" />,
-		cssName: "git"
-	},
-	{
-		name: 'java',
-		icon: <BiLogoJava size="25px" color="white" />,
-		cssName: "java"
-	},
-	{
-		name: 'C++',
-		icon: <TbBrandCpp size="25px" color="white" />,
-		cssName: "cpp"
-	},
-	{
-		name: 'Problem Solving',
-		icon: <BsPuzzle size="25px" color="white" />,
-		cssName: "problem-solving"
+		name: "Tailwind.css Logo",
+		url: "https://camo.githubusercontent.com/95759dac505a57f5a260db91eca6f7a0c852a095cb271cc6d37c413081c5f799/68747470733a2f2f696d672e736869656c64732e696f2f62616467652f5461696c77696e645f4353532d3338423241433f7374796c653d666f722d7468652d6261646765266c6f676f3d7461696c77696e642d637373266c6f676f436f6c6f723d7768697465"
 	}
 ];
 
 const projects = [
 	{
-		name: 'Global Share',
-		link: 'https://ibrahimhiarea.github.io/Global-Share/#/home',
-		github: 'https://github.com/IbrahimHiarea/Global-Share',
-		description: 'The Global Share ERP System is an innovative web-based application designed to streamline volunteer recruitment, management, and reward systems. It incorporates task management, recruitment, and gamification features to enhance employee engagement, promote effective communication, and drive organizational excellence.',
-		image: GlobalShare
+		name: '.Net/Fullstack Developer',
+		location: "Remote",
+		alt: "ASRC Federal Logo",
+		image: Asrc,
+		dates: "July 2024 - Present",
+		tasks: [
+			"Maintained and developed RESTful API endpoints within a .NET framework",
+			"Validated API functionality through unit testing as the use of Swagger",
+			"Followed industry-standard deprecation practices for legacy endpoints",
+			"Collaborated closely with QA to identify, track, and resolve defects",
+			"Adapted to evolving requirements while working in an Agile environment"
+		  ]
 	},
 	{
-		name: `Ata'a`,
-		link: 'https://ibrahimhiarea.github.io/Ataa-Dashboard/',
-		github: 'https://github.com/IbrahimHiarea/Ataa-Dashboard',
-		description: "Ataa is a web application built with React for managing and organizing the operations of a charity organization. The application includes a landing page that provides an overview of the charity's mission and goals. Staff members of the charity can log in to the main dashboard, which allows them to manage and control the projects, employees, and beneficiaries associated with the charity. Additionally, there is a section dedicated to viewing the total budget of the charity, as well as the budget allocated to each individual project.",
-		image: Ataa
+		name: 'Software Developer',
+		location: "Logan, Utah",
+		alt: "Maiden Voyage Logo",
+		image: Maidenvoyage,
+		dates: "November 2021 - July 2024",
+		tasks: [
+			"Developed and maintained web applications using Laravel and Vue.js, both from scratch and within existing codebases",
+			"Contributed to Ionic-based mobile app projects, supporting cross-platform functionality",
+			"Led onboarding and training for new team members to ensure smooth integration and knowledge transfer",
+			"Conducted thorough code reviews to uphold code quality, consistency, and best practices",
+			"Collaborated directly with clients to gather requirements and define project goals"
+		  ]
 	},
 	{
-		name: 'SoKo Number',
-		link: 'https://ibrahimhiarea.github.io/Soko-Number/',
-		github: 'https://github.com/IbrahimHiarea/Soko-Number',
-		description: 'Soko Number is a puzzle game built with the React framework. The game consists of 6 challenging levels that will test your problem-solving skills. Each level presents a grid-based puzzle where you need to move numbered tiles to their designated positions.',
-		image: SokoNumber
-	},
-	{
-		name: 'Leon',
-		link: 'https://ibrahimhiarea.github.io/Leon/',
-		github: 'https://github.com/IbrahimHiarea/Leon',
-		description: 'Leon is a modern and responsive HTML and CSS design template that provides a sleek and visually appealing foundation for building stunning websites. With its clean and elegant design, Leon focuses on simplicity and exceptional user experience.',
-		image: Leon
-	},
-	{
-		name: 'Kasper',
-		link: 'https://ibrahimhiarea.github.io/Kasper/',
-		github: 'https://ibrahimhiarea.github.io/Kasper/',
-		description: 'Kaspar is a modern and responsive HTML and CSS design template that provides a sleek and visually appealing foundation for building stunning websites. With its clean and elegant design, Kaspar focuses on simplicity and exceptional user experience.',
-		image: Kasper
-	},
-	{
-		name: 'Elzero',
-		link: 'https://ibrahimhiarea.github.io/Elzero/',
-		github: 'https://github.com/IbrahimHiarea/Elzero',
-		description: 'Elzero is a stylish and interactive HTML, CSS, and JavaScript design template that offers a modern and engaging user experience. With its clean code structure and well-designed components, Elzero provides a solid foundation for building dynamic and visually appealing web applications.',
-		image: Elzero
-	},
+		name: 'Web Developer',
+		location: "San Diego, California",
+		alt: "Agent Elite Logo",
+		image: Agentelite,
+		dates: "December 2019 - August 2021", 
+		tasks: [
+			"Developed and customized WordPress themes, plugins, shortcodes, and metaboxes to meet business requirements",
+			"Maintained and updated client websites with CSS and jQuery enhancements",
+			"Implemented a custom PTO request workflow within the company time clock system using WordPress and PHP",
+			"Supported the launch and configuration of new servers to ensure successful deployments",
+			"Collaborated with internal teams to deliver reliable and scalable WordPress solutions"
+		  ]
+	}
 ]
 
 function App() {
@@ -177,8 +132,7 @@ function App() {
 			{/* Navbar */}
 			<div className={style.nav}>
 				<a className={style.logo}>
-					<FaReact color='var(--primary-main)' size='50px' />
-					<h5>ibrahim hiarea</h5>
+					<h5>Paige MacGregor</h5>
 				</a>
 				<ul>
 					<li><a href="#Home">Home</a></li>
@@ -208,8 +162,8 @@ function App() {
 			{/* Home */}
 			<div id='Home' className={style.home}>
 				<div className={style["home-content"]}>
-					<h1>HEY, I'M Ibhrahim Hiarea</h1>
-					<p>A Frontend focused Web Developer building the Frontend of Websites and Web Applications that leads to the success of the overall product</p>
+					<h1>Hey, I'm <span className={style["home-italic"]}>Paige MacGregor</span></h1>
+					<p>A Full Stack Developer, skilled at building and maintaining frontends, backends, and databases for websites and applications that lead to the success of the overall product</p>
 					<a
 						href={cv}
 						download="cv-PDF-document"
@@ -217,7 +171,7 @@ function App() {
 						rel="noopener noreferrer"
 					>
 						<DownloadButton >
-							Download CV
+							Download Resume
 						</DownloadButton>
 					</a>
 				</div>
@@ -230,17 +184,11 @@ function App() {
 					</div>
 				</div>
 				<div className={style["contact-nav"]}>
-					<a className={style.github} target="_blank" href='https://github.com/IbrahimHiarea' >
+					<a className={style.github} target="_blank" href='https://github.com/paigem33' >
 						<AiFillGithub size="30px" color='black' />
 					</a>
-					<a className={style.linkedin} target="_blank" href='https://www.linkedin.com/in/ibrahim-hiarea/' >
+					<a className={style.linkedin} target="_blank" href='https://www.linkedin.com/in/paige-macgregor/' >
 						<AiFillLinkedin size="30px" color='black' />
-					</a>
-					<a className={style.gmail} target="_blank" href="mailto:ibrahimhiarea2@gmail.com?subject=SendMail&body=Description" >
-						<BiLogoGmail size="30px" color='black' />
-					</a>
-					<a className={style.facebook} target="_blank" href='https://www.facebook.com/ibrahim.hiarea' >
-						<BsFacebook size="30px" color='black' />
 					</a>
 				</div>
 			</div>
@@ -248,30 +196,28 @@ function App() {
 			{/* About */}
 			<div id='About' className={style.about}>
 				<div className={style.container}>
-					<h2 className={style.title}>About Me</h2>
-					<p>Here you will find more information about me, what I do, and my current skills mostly in terms of programming and technology</p>
 					<div className={style["about-content"]}>
 						<div className={style["about-info"]}>
-							<h3>Get to know me!</h3>
+							<h3 className={style.title}>About Me</h3>
 							<p>
-								I'm a <span>Frontend Web Developer</span> building the Front-end of Websites and Web Applications that leads to the success of the overall product. Check out some of my work in the <span>Projects</span> section. <br /> <br />
-								I also like sharing content related to the stuff that I have learned over the years in <span>Web Development</span> so it can help other people of the Dev Community. Feel free to Connect or Follow me on my <a href="https://github.com/IbrahimHiarea" target="_blank">Github</a> where I post useful content related to Web Development and Programming. <br /> <br />
-								I'm open to <span>Job</span> opportunities where I can contribute, learn and grow. If you have a good opportunity that matches my skills and experience then don't hesitate to <span>contact</span> me.
+								I have been working and studying in the tech world for about 7 years, and I have loved every second of it, both the satisfying and the frustrating ones! I enjoy having the opportunity to learn new skills and solve new problems everyday, one line of code at a time. <br /> <br />
+								In 2019, I graduated from a coding bootcamp in San Diego, California and got my first full time job as a developer shortly there after. I was there for about two years, and during that time I realized this was what I loved and wanted to do forever. That led me to enroll at Utah State University and graduate with a Computer Science degree in 2024. During that time I had the opportunity to start working at another amazing company, which meant I was able to grow my skill set both in and out of the classroom. Upon graduation, my education and previous experience allowed me to start working in my current role at ASRC Federal.<br /> <br />
+								When I'm not behind a computer, you can normally find me either with my nose in a book, out on a hike, rewatching Star Trek TNG for the tenth time, or setting up camp for a weekend in the mountains.
 							</p>
-						</div>
-						<div className={style["my-skill"]}>
-							<h3>My Skills</h3>
-							<div className={style.skills}>
-								{
-									skills.map((skill, index) => {
-										return <div key={`skill${index}`} className={`${style.skill} ${style[skill.cssName]}`}>
-											<div className={style["skill-name"]}>{skill.name}</div>
-											<div className={style["skill-icon"]}>{skill.icon}</div>
-										</div>
-									})
-								}
+							<div className={style["my-skill"]}>
+								<h3>Some of my favorites:</h3>
+								<div className={style.skills}>
+									{
+										skills.map((skill, index) => {
+											return <div key={`skill${index}`} className={style.skill}>
+												<img src={skill.url} alt={skill.name}/>
+											</div>
+										})
+									}
+								</div>
 							</div>
 						</div>
+						<div className={style["about-image"]}></div>
 					</div>
 				</div>
 			</div>
@@ -279,40 +225,26 @@ function App() {
 			{/* Projects */}
 			<div id='Projects' className={style.projects}>
 				<div className={style.container}>
-					<h2 className={style.title}>Projects</h2>
-					<p>Here you will find some of the personal and clients projects that I created with each project containing its own case study</p>
+					<h2 className={style.title}>Experience</h2>
 					<div className={style["projects-list"]}>
 						{
 							projects.map((project, index) => {
-								return <div key={`project${index}`} className={style.project}>
+								return <div key={`project-${index}`} className={style.project}>
 									<div className={style["project-image"]}>
-										<img src={project.image} alt="Project Image" />
+										<img src={project.image} alt={project.alt}/>
 									</div>
 									<div className={style["project-info"]}>
 										<h3>{project.name}</h3>
-										<p>{project.description}</p>
-										<div className={style["project-buttons"]}>
-											<IconButton
-												width="170px"
-												height="50px"
-												backgroundColor="var(--primary-main)"
-												color="white"
-												link={project.link}
-												icon={<AiOutlineEye size="25px" color='white' />}
-											>
-												Live Demo
-											</IconButton>
-											<IconButton
-												width="100px"
-												height="50px"
-												backgroundColor="black"
-												color="white"
-												link={project.github}
-												icon={<AiFillGithub size="25px" color='white' />}
-											>
-												Github
-											</IconButton>
-										</div>
+										<p>{project.dates}</p>
+										<p>{project.location}</p>
+										<ul className={style["project-tasks"]} >
+										{
+											project.tasks.map((task, index) => {
+												return <li key={`task-${index}`}>{task}</li>
+												
+											})
+										}
+										</ul>
 									</div>
 								</div>
 							})
@@ -382,29 +314,20 @@ function App() {
 				<div className={style.container}>
 					<div className={style["footer-info"]}>
 						<div>
-							<h3>ibrahim hiarea</h3>
-							<p>A Frontend focused Web Developer building the Frontend of Websites and Web Applications that leads to the success of the overall product</p>
+							<h3>Paige MacGregor</h3>
+							<p>A Full Stack Developer, skilled at building the frontend, backend, and databases of websites and applications that lead to the success of the overall product</p>
 						</div>
 						<div className={style.social}>
 							<h3>Social</h3>
 							<div className="">
-								<a className={style.git} target="_blank" href='https://github.com/IbrahimHiarea' >
+								<a className={style.git} target="_blank" href='https://github.com/paigem33' >
 									<AiFillGithub size="30px" color='white' />
 								</a>
-								<a className={style.linkedin} target="_blank" href='https://www.linkedin.com/in/ibrahim-hiarea/' >
+								<a className={style.linkedin} target="_blank" href='https://www.linkedin.com/in/paige-macgregor/' >
 									<AiFillLinkedin size="30px" color='white' />
-								</a>
-								<a className={style.gmail} target="_blank" href="mailto:ibrahimhiarea2@gmail.com?subject=SendMail&body=Description" >
-									<BiLogoGmail size="30px" color='white' />
-								</a>
-								<a className={style.facebook} target="_blank" href='https://www.facebook.com/ibrahim.hiarea' >
-									<BsFacebook size="30px" color='white' />
 								</a>
 							</div>
 						</div>
-					</div>
-					<div className={style["copy-right"]}>
-						© Copyright 2023. Made by <span>Ibrahim Hiarea</span>
 					</div>
 				</div>
 			</div>
