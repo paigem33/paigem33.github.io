946d3781465e45aa744447603ff61fc77836e8ee
Paige MacGregor <paigemacgregor33@gmail.com>
2025-07-22 15:50:47 -0600

    update mobile styles

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -214,9 +214,10 @@ function App() {
 					<h5>Paige MacGregor</h5>
 				</a>
 				<ul>
-					<li><a href="#Home">Home</a></li>
+				<li><a href="#Home">Home</a></li>
 					<li><a href="#About">About</a></li>
-					<li><a href="#Projects">Projects</a></li>
+					<li><a href="#Experience">Experience</a></li>
+					<li><a href="#Education">Education</a></li>
 					<li><a href="#Contact">Contact</a></li>
 				</ul>
 				<div className={style["menu-icon"]}>
